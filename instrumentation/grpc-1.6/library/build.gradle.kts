bc3d03911bc17164d29c7708374cef535788d881
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2021-10-27 17:40:43 -0400

    Bump version.

--- instrumentation/grpc-1.6/library/build.gradle.kts
+++ instrumentation/grpc-1.6/library/build.gradle.kts
@@ -7,7 +7,7 @@ val grpcVersion = "1.6.0"
 dependencies {
   library("io.grpc:grpc-core:$grpcVersion")
 
-  library("cloud.filibuster:instrumentation:0.12-SNAPSHOT")
+  library("cloud.filibuster:instrumentation:0.14-SNAPSHOT")
 
   testLibrary("io.grpc:grpc-netty:$grpcVersion")
   testLibrary("io.grpc:grpc-protobuf:$grpcVersion")
