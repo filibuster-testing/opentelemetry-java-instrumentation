fff168be718dd5efa1df109dfdf820f6ece8bc2c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2022-02-10 12:32:13 -0500

    Bump HTTP instrumentation client and server.

--- instrumentation/armeria-1.3/library/src/main/java/io/opentelemetry/instrumentation/armeria/v1_3/OpenTelemetryFilibusterDecoratingHttpService.java
+++ instrumentation/armeria-1.3/library/src/main/java/io/opentelemetry/instrumentation/armeria/v1_3/OpenTelemetryFilibusterDecoratingHttpService.java
@@ -14,52 +14,52 @@ import javax.annotation.Nullable;
 import java.util.logging.Logger;
 
 public class OpenTelemetryFilibusterDecoratingHttpService extends FilibusterDecoratingHttpService {
-  private static final Logger logger = Logger.getLogger(OpenTelemetryFilibusterDecoratingHttpService.class.getName());
+    private static final Logger logger = Logger.getLogger(OpenTelemetryFilibusterDecoratingHttpService.class.getName());
 
-  @Nullable
-  private final Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter;
+    @Nullable
+    private final Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter;
 
-  @SuppressWarnings("NullAway")
-  private Context context;
+    @SuppressWarnings("NullAway")
+    private Context context;
 
-  public OpenTelemetryFilibusterDecoratingHttpService(HttpService delegate, String serviceName, Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter) {
-    super(delegate);
-    this.serviceName = serviceName;
-    this.serverInstrumenter = serverInstrumenter;
-    this.contextStorage = new OpenTelemetryContextStorage();
-  }
-
-  public OpenTelemetryFilibusterDecoratingHttpService(HttpService delegate, Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter) {
-    super(delegate);
-    this.serviceName = System.getenv("SERVICE_NAME");
-    this.serverInstrumenter = serverInstrumenter;
-    this.contextStorage = new OpenTelemetryContextStorage();
-  }
-
-  @Override
-  protected void setupContext(ServiceRequestContext ctx, HttpRequest req) {
-    OpenTelemetryContextStorage openTelemetryContextStorage = (OpenTelemetryContextStorage) this.contextStorage;
-    context = openTelemetryContextStorage.getContext();
-
-    if (serverInstrumenter != null) {
-      context = serverInstrumenter.start(context, ctx);
+    public OpenTelemetryFilibusterDecoratingHttpService(HttpService delegate, String serviceName, Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter) {
+        super(delegate);
+        this.serviceName = serviceName;
+        this.serverInstrumenter = serverInstrumenter;
+        this.contextStorage = new OpenTelemetryContextStorage();
     }
-  }
 
-  @Override
-  protected void contextWhenComplete(ServiceRequestContext ctx) {
-    ctx.log().whenComplete().thenAccept(log -> {
-      if (serverInstrumenter != null) {
-        serverInstrumenter.end(context, ctx, log, log.responseCause());
-      }
-    });
-  }
-
-  @Override
-  protected HttpResponse delegateWithContext(ServiceRequestContext ctx, HttpRequest req) throws Exception {
-    try (Scope ignored = context.makeCurrent()) {
-      HttpService delegate = (HttpService) unwrap();
-      return delegate.serve(ctx, req);
+    public OpenTelemetryFilibusterDecoratingHttpService(HttpService delegate, Instrumenter<ServiceRequestContext, RequestLog> serverInstrumenter) {
+        super(delegate);
+        this.serviceName = System.getenv("SERVICE_NAME");
+        this.serverInstrumenter = serverInstrumenter;
+        this.contextStorage = new OpenTelemetryContextStorage();
     }
-  }
+
+    @Override
+    protected void setupContext(ServiceRequestContext ctx, HttpRequest req) {
+        OpenTelemetryContextStorage openTelemetryContextStorage = (OpenTelemetryContextStorage) this.contextStorage;
+        context = openTelemetryContextStorage.getContext();
+
+        if (serverInstrumenter != null) {
+            context = serverInstrumenter.start(context, ctx);
+        }
+    }
+
+    @Override
+    protected void contextWhenComplete(ServiceRequestContext ctx) {
+        ctx.log().whenComplete().thenAccept(log -> {
+            if (serverInstrumenter != null) {
+                serverInstrumenter.end(context, ctx, log, log.responseCause());
+            }
+        });
+    }
+
+    @Override
+    protected HttpResponse delegateWithContext(ServiceRequestContext ctx, HttpRequest req) throws Exception {
+        try (Scope ignored = context.makeCurrent()) {
+            HttpService delegate = (HttpService) unwrap();
+            return delegate.serve(ctx, req);
+        }
+    }
 }
